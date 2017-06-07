a64f6267cbd24e8abfd03706844270b1082b2e89
hnzhangyang <qwe7414287@sina.cn>
2017-06-07 10:25:37 +0800

    Generators

--- Generators/ch.js
+++ Generators/ch.js
@@ -1,19 +1,13 @@
 function* generator(){
-    console.log('0-1')
     yield 1
-    console.log('1-1')
     yield 2
-    console.log('2-1')
     yield 3
-    console.log('3-3')
+    return
+    yield 4
+    yield 5
 }
 
 var g = generator()
 
-console.log(g.next())
-
-console.log(g.next())
-
-console.log(g.next())
-
-console.log(g.next())
+console.log(...g)
+// 1 2 3
